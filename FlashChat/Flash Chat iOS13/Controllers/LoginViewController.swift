e066d08d8f39f816adcc0c2ae27c4787638f270f
Berkay Kuzu <92036779+Berkay-Kuzu@users.noreply.github.com>
2022-10-09 23:10:17 +0300

    Add files via upload

---
+++ FlashChat/Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -0,0 +1,28 @@
+
+
+import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+import FirebaseStorage
+import FirebaseAnalytics
+
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+
+    @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e)
+                } else {
+                    self.performSegue(withIdentifier: K.loginSegue, sender: self)
+                }
+            }
+        }
+    }
+}
---
+++ FlashChat/Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -0,0 +1,30 @@
+
+
+import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+import FirebaseStorage
+import FirebaseAnalytics
+
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    @IBAction func registerPressed(_ sender: UIButton) {
+       
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e)
+                } else {
+                    //Navigate to the ChatViewController
+                    self.performSegue(withIdentifier: K.registerSegue, sender: self)
+                }
+            }
+        }
+    }
+    
+}
